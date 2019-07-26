d451a9540a4cc89be8399b9ac6bff168a7656de0
Felipe Teixeira Moraes <fmoraes@mastertech.tech>
2019-07-26 17:30:01 -0300

    Adicionar testes unitários de Conta

--- src/test/java/com/br/zup/models/ContaTeste.java
+++ src/test/java/com/br/zup/models/ContaTeste.java
@@ -2,6 +2,7 @@ package com.br.zup.models;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -10,8 +11,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class ContaTeste {
 	private Conta conta;
 	
-	@Test
-	public void testarDepositarContaCorrente() {
+	@Before
+	public void preparar() {
 		conta = new Conta();
 		
 		conta.setId(null);
@@ -21,9 +22,46 @@ public class ContaTeste {
 		conta.setDigito(1);
 		
 		conta.setSaldoContaCorrente(100D);
-		conta.setSaldoContaPoupanca(0D);
-		
+		conta.setSaldoContaPoupanca(100D);
+	}
+	
+	@Test
+	public void testarDepositarContaCorrente() {
 		conta.depositarContaCorrente(100D);
+		assertEquals(200D, conta.getSaldoContaCorrente(), 0D);
+	}
+	
+	@Test
+	public void testarDepositarContaPoupanca() {
+		conta.depositarContaPoupanca(100D);
+		assertEquals(200D, conta.getSaldoContaPoupanca(), 0D);
+	}
+	
+	@Test
+	public void testarSacarContaCorrente() {
+		double valorSacado = conta.sacarContaCorrente(100D);
+		assertEquals(0D, conta.getSaldoContaCorrente(), 0D);
+		assertEquals(100D, valorSacado, 0D);
+	}
+	
+	@Test
+	public void testarSacarContaPoupanca() {
+		double valorSacado = conta.sacarContaPoupanca(100D);
+		assertEquals(0D, conta.getSaldoContaPoupanca(), 0D);
+		assertEquals(100D, valorSacado, 0D);
+	}
+	
+	@Test
+	public void testarTransferirCorrenteParaPoupanca() {
+		conta.transferirCorrenteParaPoupanca(100D);
+		assertEquals(0D, conta.getSaldoContaCorrente(), 0D);
+		assertEquals(200D, conta.getSaldoContaPoupanca(), 0D);
+	}
+	
+	@Test
+	public void testarTransferirPoupancaParaCorrente() {
+		conta.transferirPoupancaParaCorrente(100D);
+		assertEquals(0D, conta.getSaldoContaPoupanca(), 0D);
 		assertEquals(200D, conta.getSaldoContaCorrente(), 0D);
 	}
 }
