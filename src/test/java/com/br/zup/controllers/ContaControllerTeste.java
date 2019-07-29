3da12bc2ad397e8d3cdecd69f6b70b830ec42a79
Felipe Teixeira Moraes <fmoraes@mastertech.tech>
2019-07-29 15:45:06 -0300

    Adicionar teste de noContent do método GET

--- src/test/java/com/br/zup/controllers/ContaControllerTeste.java
+++ src/test/java/com/br/zup/controllers/ContaControllerTeste.java
@@ -3,6 +3,7 @@ package com.br.zup.controllers;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.br.zup.models.Conta;
 import com.br.zup.services.ContaService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.mockito.BDDMockito.given;
@@ -28,20 +30,25 @@ public class ContaControllerTeste {
 	@MockBean
 	private ContaService contaService;
 	
+	private Conta conta;
+	
+	@Before
+	public void preparar() {
+		// Criar uma resposta esperada por .findAll()
+				conta = new Conta();
+				
+				conta.setId(1);
+				conta.setBanco("6666 - Banco com Dinheiros");
+				conta.setAgencia(4444);
+				conta.setConta(11111);
+				conta.setDigito(1);
+				
+				conta.setSaldoContaCorrente(100D);
+				conta.setSaldoContaPoupanca(100D);
+	}
+	
 	@Test
 	public void testarPegarContas() throws Exception {
-		// Criar uma resposta esperada por .findAll()
-		Conta conta = new Conta();
-		
-		conta.setId(1);
-		conta.setBanco("6666 - Banco com Dinheiros");
-		conta.setAgencia(4444);
-		conta.setConta(11111);
-		conta.setDigito(1);
-		
-		conta.setSaldoContaCorrente(100D);
-		conta.setSaldoContaPoupanca(100D);
-		
 		List<Conta> contas = Arrays.asList(conta);
 		
 		given(contaService.findAll()).willReturn(contas);
@@ -51,5 +58,22 @@ public class ContaControllerTeste {
 						.andExpect(status().isOk())
 						.andExpect(jsonPath("$", hasSize(1)))
 						.andExpect(jsonPath("$[*].id").isNotEmpty());
+	}
+	
+	@Test
+	public void testarPegarContasErro() throws Exception {
+		given(contaService.count()).willReturn(0L);
+		
+		this.mockMvc.perform(get("/api/conta"))
+						.andExpect(status().isNoContent());
+	}
+	
+	public static String comoJson(final Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
